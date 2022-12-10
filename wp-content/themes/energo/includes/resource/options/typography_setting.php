efef7104d0ae6cd9fc7d2cee5f464beeb77142b1
SlagriSolar <slagrisolar@gmail.com>
2022-12-10 09:54:33 -0800

    images

--- wp-content/themes/energo/includes/resource/options/typography_setting.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-
-    'title'         => esc_html__( 'Typography Settings', 'energo' ),
-    'id'            => 'typography_setting',
-    'desc'          => '',
-    'icon'          => 'el el-edit'
-);
